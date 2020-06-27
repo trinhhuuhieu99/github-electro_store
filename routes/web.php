7a3b22a24b5f76a06dbdc5f027e35c36e869aa7f
trinhhuuhieu99@gmail.com <trinhhuuhieu99@gmail.com>
2020-06-27 15:42:45 +0700

    begin the project

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test', function () {
-    return('aaaaa');
-});
-
-Route::get('users/{id}', function ($id) {
-    
-});
