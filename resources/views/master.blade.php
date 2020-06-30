f0954e9a627366ddaf240956401b416cd635f626
trinhhuuhieu99@gmail.com <trinhhuuhieu99@gmail.com>
2020-06-30 12:19:04 +0700

    update 3

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -60,7 +60,15 @@
 <script type="text/javascript" src="source/js/bootstrap-3.1.1.min.js"></script>
 <!-- //for bootstrap working -->
 <script src="source/js/easyResponsiveTabs.js" type="text/javascript"></script>
-
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#horizontalTab').easyResponsiveTabs({
+            type: 'default', //Types: default, vertical, accordion           
+            width: 'auto', //auto or any width like 600px
+            fit: true   // 100% fit in a container
+        });
+    });
+</script>
 <!-- flexslider -->
 <script defer src="source/js/jquery.flexslider.js"></script>
 <script>
