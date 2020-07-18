ff589773331b70c13270967bfe8eb36f62ce64e2
trinhhuuhieu99@gmail.com <trinhhuuhieu99@gmail.com>
2020-07-19 04:01:05 +0700

    update model

--- app/BillDetail.php
+++ app/BillDetail.php
@@ -9,4 +9,14 @@ class BillDetail extends Model
     
     protected $table = "bill_detail";
     
+    public function product()
+    {
+        return $this->belongsTo('App\Product', 'id_product ', 'id');
+    }
+
+    public function bill()
+    {
+        return $this->belongsTo('App\Bill', 'id_bill ', 'id');
+    }
+
 }
--- app/Product.php
+++ app/Product.php
@@ -8,4 +8,15 @@ class Product extends Model
 {
     //
     protected $table = "products";
+
+    public function product_type()
+    {
+        return $this->belongsTo('App\ProductType', 'id_type', 'id');
+    }
+
+    public function bill_detail()
+    {
+        return $this->hasMany('App\BillDedail', 'id_product', 'id');
+    }
+
 }
